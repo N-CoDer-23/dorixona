901b58095a993add7a086ee0803e92f553c9968b
Nuriddin2004 <ortiqboevnuriddin1@gmail.com>
2024-03-12 10:07:39 +0500

    one time

---
+++ backend/model/UserSchema.js
@@ -0,0 +1,19 @@
+const { Schema, model}= require('mongoose')
+
+const userSchema = new Schema({ 
+    fname: {type: String, require: true},
+    lname: {type: String, require: true},
+    birthday: {type: String, require: true},
+    userName: {type: String, require: true},
+    password: {type: String, require: true},
+    gender: {type: String},
+    phoneNumber: {type: Number, require: true},
+    address: {type: String},
+    salary: {type: String, require: true},
+    idNumber: {type: String, require: true},
+    type: {type: String, require: true},
+    workTime: {type: String, require: true}
+})
+
+const User = model('User', userSchema);
+module.exports = User;
